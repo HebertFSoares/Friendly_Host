4658eb91e1dec24e98fa1764e210e32a85c633d5
Hebert Soares <hebertsoaresof@gmail.com>
2023-05-06 22:22:11 -0300

    Criação das models do cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+        confirma_senha = request.POST.get("confirma_senha")
+        
+        if not (senha == confirma_senha):
+            return redirect(reverse('cadastro'))
+        
+        #TODO: adicionar mais verificações!
+        
