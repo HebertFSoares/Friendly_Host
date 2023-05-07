4a97640aa5b7c1519e222e1ca6f48c29acfbb57a
Hebert Soares <hebertsoaresof@gmail.com>
2023-05-07 17:25:40 -0300

    Adicionando um botão para modo escuro

--- templates/static/cadastro/js/cadastro.js
+++ templates/static/cadastro/js/cadastro.js
@@ -15,4 +15,18 @@ document.getElementById("user-type").addEventListener("change", function() {
       anfitriaoFields.style.display = "none";
     }
   });
+
+function toggleDarkMode() {
+    var body = document.body;
+    body.classList.toggle('dark-mode');
+  }
+  
+  // Selecionar o botão de alternar modo escuro
+  var darkModeButton = document.getElementById('dark-mode-button');
+  
+  // Adicionar evento de clique no botão
+  darkModeButton.addEventListener('click', function() {
+    toggleDarkMode();
+  });
+  
   
