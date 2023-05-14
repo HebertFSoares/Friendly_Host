20269841ee8d26bf63e638acc59d5bdaab6f8cfc
Hebert Soares <hebertsoaresof@gmail.com>
2023-05-14 20:18:32 -0300

    Atualizações da views

--- home/views.py
+++ home/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from usuarios.models import Perfil
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 
 @login_required
 def home_estudante(request):
@@ -11,9 +10,8 @@ def home_estudante(request):
         if perfil.user_type == 'estudante':
             return render(request, 'home_estudante.html')
         else:
-            return render(request, 'erro.html')  # Redirecionar para a página de acesso negado
+            return render(request, 'erro.html')
     except Perfil.DoesNotExist:
-        # Caso o perfil não exista, redirecionar para a página de acesso negado
         return render(request, 'erro.html')
 
 @login_required
@@ -23,7 +21,6 @@ def home_anfitriao(request):
         if perfil.user_type == 'anfitriao':
             return render(request, 'home_anfitriao.html')
         else:
-           return render(request, 'erro.html')  # Redirecionar para a página de acesso negado
+           return render(request, 'erro.html')  
     except Perfil.DoesNotExist:
-        # Caso o perfil não exista, redirecionar para a página de acesso negado
         return render(request, 'erro.html')
