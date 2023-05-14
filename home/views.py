785ab997be9a4ddd8c95e9984e822894d7bf8adc
Hebert Soares <hebertsoaresof@gmail.com>
2023-05-14 20:08:04 -0300

    Criação da tela de erro

--- home/views.py
+++ home/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from usuarios.models import Perfil
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 @login_required
 def home_estudante(request):
-    pefil = request.user.perfil
-    if pefil.user_type == 'estudante':
-        return render(request, 'home_estudante.html')
-    else:
+    try:
+        perfil = request.user.perfil
+        if perfil.user_type == 'estudante':
+            return render(request, 'home_estudante.html')
+        else:
+            return render(request, 'erro.html')  # Redirecionar para a página de acesso negado
+    except Perfil.DoesNotExist:
+        # Caso o perfil não exista, redirecionar para a página de acesso negado
         return render(request, 'erro.html')
 
 @login_required
 def home_anfitriao(request):
-    pefil = request.user.perfil
-    if pefil.user_type == 'estudante':
-        return render(request, 'home_anfitriao.html')
-    else:
+    try:
+        perfil = request.user.perfil
+        if perfil.user_type == 'anfitriao':
+            return render(request, 'home_anfitriao.html')
+        else:
+           return render(request, 'erro.html')  # Redirecionar para a página de acesso negado
+    except Perfil.DoesNotExist:
+        # Caso o perfil não exista, redirecionar para a página de acesso negado
         return render(request, 'erro.html')
