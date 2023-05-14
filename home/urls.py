677400b6ffdc44919104b8aaf956d3c3ced0f8b2
Hebert Soares <hebertsoaresof@gmail.com>
2023-05-14 19:58:23 -0300

    Restringindo a tela home do estudante e anfitrião

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+
 urlpatterns = [
     path('estudante/', views.home_estudante, name="home_estudante"),
+    path('anfitriao/', views.home_anfitriao, name='anfitriao_home'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render,redirect
 from django.urls import reverse
+from usuarios.models import Perfil
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def home_estudante(request):
-    return render(request, 'home.html')
+    pefil = request.user.perfil
+    if pefil.user_type == 'estudante':
+        return render(request, 'home_estudante.html')
+    else:
+        return render(request, 'erro.html')
+
+@login_required
+def home_anfitriao(request):
+    pefil = request.user.perfil
+    if pefil.user_type == 'estudante':
+        return render(request, 'home_anfitriao.html')
+    else:
+        return render(request, 'erro.html')
