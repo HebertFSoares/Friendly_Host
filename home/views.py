56dcf2d806493a5bca85cb9f1196a924f1e01bc6
Hebert Soares <hebertsoaresof@gmail.com>
2023-05-18 23:12:13 -0300

    Adicionando a tela de erro nas views de home

--- home/views.py
+++ home/views.py
@@ -8,21 +8,21 @@ def is_estudante(user):
         perfil = Perfil.objects.get(usuario=user)
         return perfil.user_type == 'estudante'
     except Perfil.DoesNotExist:
-        return redirect(reverse('erro.html'))
+        return False
 
 def is_anfitriao(user):
     try:
         perfil = Perfil.objects.get(usuario=user)
         return perfil.user_type == 'anfitriao'
     except Perfil.DoesNotExist:
-        return redirect(reverse('erro.html'))
+        return False
 
 @login_required
-@user_passes_test(is_estudante)
+@user_passes_test(is_estudante, login_url='erro.html')
 def home_estudante(request):
     return render(request, 'home_estudante.html')
 
 @login_required
-@user_passes_test(is_anfitriao)
+@user_passes_test(is_anfitriao, login_url='erro.html')
 def home_anfitriao(request):
     return render(request, 'home_anfitriao.html')
