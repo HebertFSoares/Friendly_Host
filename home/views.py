29ff367af39ddc3480f0ee464d7ac905d674a212
Hebert Soares <hebertsoaresof@gmail.com>
2023-05-21 23:30:25 -0300

    Construção das views adicionar_vaga

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required
 from usuarios.models import Perfil
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from .forms import AdicionarVagaForm
+from .models import VagaCasa
+
 
 def is_estudante(user):
     try:
@@ -33,5 +36,38 @@ def home_anfitriao(request):
 def error(request):
     return render(request, 'erro.html')
 
+from django.shortcuts import render, redirect
+from .forms import AdicionarVagaForm
+from .models import VagaCasa
+
 def adicionar_vaga(request):
-    pass
+    if request.method == 'POST':
+        form = AdicionarVagaForm(request.POST)
+        if form.is_valid():
+            # Processar os dados do formulário e criar uma nova vaga de casa
+            titulo = form.cleaned_data['titulo']
+            descricao = form.cleaned_data['descricao']
+            localizacao = form.cleaned_data['localizacao']
+            capacidade = form.cleaned_data['capacidade']
+            regras_casa = form.cleaned_data['regras_casa']
+            informacoes_familia = form.cleaned_data['informacoes_familia']
+            contato = form.cleaned_data['contato']
+            
+            vaga = VagaCasa(
+                titulo=titulo,
+                descricao=descricao,
+                localizacao=localizacao,
+                capacidade=capacidade,
+                regras_casa=regras_casa,
+                informacoes_familia=informacoes_familia,
+                contato=contato
+            )
+            vaga.save()
+            
+            # Redirecionar para a página de sucesso ou exibir uma mensagem de confirmação
+            return redirect('pagina_sucesso')  # substitua 'pagina_sucesso' pela URL correta da página de sucesso
+        
+    else:
+        form = AdicionarVagaForm()
+    
+    return render(request, 'adicionar_vaga.html', {'form': form})
